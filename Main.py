6970db3a274a5e6a155956049ae3b2b46a382892
ayutaz <ka1357amnbpdr@gmail.com>
2022-05-04 13:44:27 +0900

    feat: notion do queryにfilterの追加

--- Main.py
+++ Main.py
@@ -11,7 +11,13 @@ notion = Client(auth=os.getenv("NOTION_TOKEN"))
 
 db = notion.databases.query(
     **{
-        'database_id': os.getenv("PRIVATE_DB")  # データベースID
+        'database_id': os.getenv("PRIVATE_DB"),
+        "filter": {
+            "property": "Deadline",
+            "select": {
+                "equals": 'Done'
+            }
+        }
     }
 )
 results = db['results']
@@ -19,5 +25,7 @@ for idx in range(len(results)):
     is_done = results[idx]['properties']['Deadline']['select']
     if is_done is None:
         continue
-    print(is_done['name'])
-    print('\n')
+    page_id = results[idx]['id']
+    if is_done != 'Done':
+        print(is_done['name'] + ': ' + page_id)
+        print('\n')
