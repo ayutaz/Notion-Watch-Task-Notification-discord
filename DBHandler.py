cc872af17223e29ea407af01e9ac871d154653f7
ayutaz <ka1357amnbpdr@gmail.com>
2022-05-04 17:59:53 +0900

    feat: DeadlineがDoneになっていて、dataがnullの場合に時刻の自動入力

---
+++ DBHandler.py
@@ -0,0 +1,60 @@
+import datetime
+
+from notion_client import Client
+
+
+class DBHandler:
+    def __init__(self, notion_token):
+        self.notion_token = notion_token
+        self.notion = Client(auth=self.notion_token)
+
+    def get_done_task_db(self, db_id):
+        db = self.notion.databases.query(
+            **{
+                'database_id': db_id,
+                "filter": {
+                    "and": [
+                        {
+                            "property": "Deadline",
+                            "select": {
+                                "equals": 'Done'
+                            }
+                        },
+                        {
+                            "property": "日付",
+                            "date": {
+                                "is_empty": True
+                            }
+                        }
+
+                    ]
+                }
+            }
+        )
+        return db['results']
+
+    def update_task_date(self, page_id):
+        today = str(datetime.datetime.now().isoformat())
+        self.notion.pages.update(
+            page_id,
+            properties={
+                '日付': {
+                    'date': {
+                        'start': today,
+                        'time_zone': 'Asia/Tokyo'
+                    }
+                }
+            }
+        )
+
+    @staticmethod
+    def get_done_date(db_result):
+        return db_result['properties']['日付']['date']
+
+    @staticmethod
+    def get_task_name(db_result):
+        return db_result['properties']['タスク名']['title'][0]['plain_text']
+
+    @staticmethod
+    def get_page_id(db_result):
+        return db_result['id']
--- Main.py
+++ Main.py
@@ -1,31 +1,24 @@
 import os
-from pprint import pprint
-
-from notion_client import Client
 
 from dotenv import load_dotenv
 
+from DBHandler import DBHandler
+
 load_dotenv()
 
-notion = Client(auth=os.getenv("NOTION_TOKEN"))
 
-db = notion.databases.query(
-    **{
-        'database_id': os.getenv("PRIVATE_DB"),
-        "filter": {
-            "property": "Deadline",
-            "select": {
-                "equals": 'Done'
-            }
-        }
-    }
-)
-results = db['results']
-for idx in range(len(results)):
-    is_done = results[idx]['properties']['Deadline']['select']
-    if is_done is None:
-        continue
-    page_id = results[idx]['id']
-    if is_done != 'Done':
-        print(is_done['name'] + ': ' + page_id)
+def TaskWatch(db_id):
+    results = db_handler.get_done_task_db(db_id)
+    for idx in range(len(results)):
+        done_date = db_handler.get_done_date(results[idx])
+        print(db_handler.get_task_name(results[idx]))
+        print(done_date)
+        page_id = db_handler.get_page_id(results[idx])
+        print(page_id)
         print('\n')
+        db_handler.update_task_date(page_id)
+
+
+db_handler = DBHandler(os.getenv("NOTION_TOKEN"))
+
+TaskWatch(os.getenv("PRIVATE_DB"))
