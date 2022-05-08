f3c1afe1e49b98394c36a9fcbd3515c1903f88f3
ayutaz <ka1357amnbpdr@gmail.com>
2022-05-08 15:27:13 +0900

    feat: Notionの「期日」(date)が特定の日時以内の場合、discordで通知を送る

--- WatchDeadTask.py
+++ WatchDeadTask.py
@@ -1,5 +1,6 @@
 import os
 
+import requests
 from dotenv import load_dotenv
 
 from DBHandler import DBHandler
@@ -11,6 +12,11 @@ def WatchDeadTask(db_id):
     results = db_handler.get_deadline_task(db_id, 3)
     for idx in range(len(results)):
         print(db_handler.get_task_name(results[idx]))
+        content = {
+            "username": "期日が迫っているタスク通知bot",
+            "content": f"「{db_handler.get_task_name(results[idx])}」が期日迫っています。"
+        }
+        requests.post(os.getenv("DISCORD_WEBHOOK"), content)
 
 
 db_handler = DBHandler(os.getenv("NOTION_TOKEN"))
