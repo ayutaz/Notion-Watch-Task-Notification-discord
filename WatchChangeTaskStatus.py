74f6fac12d7f83b5d8a5f18068d1b9e1d02993ff
ayutaz <ka1357amnbpdr@gmail.com>
2022-05-13 02:25:25 +0900

    fix: post webhook url

--- WatchChangeTaskStatus.py
+++ WatchChangeTaskStatus.py
@@ -15,9 +15,9 @@ def WatchTaskStatus():
     for task_result in db:
         if task_result is not None:
             if db_handler.is_task_status_confirm_from_doing(task_result):
-                requests.post(os.getenv("DISCORD_WEBHOOK"), message.confirm_message(task_result))
+                requests.post(os.getenv("DISCORD_WEBHOOK_TASK_STATUS"), message.confirm_message(task_result))
             elif db_handler.is_task_status_doing_from_confirm(task_result):
-                requests.post(os.getenv("DISCORD_WEBHOOK"), message.fb_message(task_result))
+                requests.post(os.getenv("DISCORD_WEBHOOK_TASK_STATUS"), message.fb_message(task_result))
 
 
 db_handler = TaskDBHandler(os.getenv("NOTION_TOKEN"))
